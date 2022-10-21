aebd2548478ca723bd7a2e1e853b05819c0650ff
techandsoju <46474702+techandsoju@users.noreply.github.com>
2022-10-20 23:28:11 -0700

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Andrew on 10/20/22.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork(){
+        print("Doing some work ...")
+    }
+}
